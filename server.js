f062f9ce9bb9fc4f65101ab30d1a4bf538f8c2e7
unknown <rashadbayar@gmail.com>
2020-05-16 23:18:32 -0700

    updated

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8081;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
